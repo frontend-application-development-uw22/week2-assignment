9a3f2e3d5b859f17a2dc2b92f96cea769582bc9b
Admin <admin@Admins-MacBook-Pro.local>
2022-04-22 22:10:40 -0700

    Fixed date formatting and adjusted byline structure.

--- blog-homepage/src/Components/Posts/PostByline.jsx
+++ blog-homepage/src/Components/Posts/PostByline.jsx
@@ -4,7 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 import { faBookmark as faBookmarkSolid } from "@fortawesome/free-solid-svg-icons";
 
+const dateHandler = (date) => {
+  const newDate = new Date(date);
+  return newDate.toDateString().slice(4, 11);
+};
+
 const PostByline = (props) => {
+  const newDate = dateHandler(props.postedDate);
+
   const [bookmarked, setBookmarked] = useState(false);
 
   const toggleBookmark = () => {
@@ -12,20 +19,24 @@ const PostByline = (props) => {
   };
 
   return (
-    <section class="byline">
-      <Image src={props.image} />
-      <section>
-        <p>{props.name}</p>
-        <p>
-          {props.postedDate} - {props.minutesToRead} min to read
-        </p>
+    <>
+      <section class="byline">
+        <Image src={props.image} />
+        <section>
+          <p>{props.name}</p>
+          <p>
+            {newDate} - {props.minutesToRead} min to read
+          </p>
+        </section>
       </section>
-      {bookmarked ? (
-        <FontAwesomeIcon icon={faBookmarkSolid} onClick={toggleBookmark} />
-      ) : (
-        <FontAwesomeIcon icon={faBookmark} onClick={toggleBookmark} />
-      )}
-    </section>
+      <div className="bookMark">
+        {bookmarked ? (
+          <FontAwesomeIcon icon={faBookmarkSolid} onClick={toggleBookmark} />
+        ) : (
+          <FontAwesomeIcon icon={faBookmark} onClick={toggleBookmark} />
+        )}
+      </div>
+    </>
   );
 };
 
