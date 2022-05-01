4d90d101489c5532f64407b2fe103f2fcc8e4ed4
RainyCityDiver <cvmembership@posteo.net>
2022-05-01 15:07:26 -0700

    Working on CSS & layout of ForYou.jsx.

--- blog-homepage/src/App.js
+++ blog-homepage/src/App.js
@@ -6,10 +6,20 @@ import ForYou from './ForYou';
 import Missed from './Missed';
 
 function App() {
+  // return (
+  //   <div>
+  //     <ForYou />
+  //     <Missed />
+  //   </div>
+  // )
   return (
     <div>
+      <div className="row row-cols-1 row-cols-sm-2 g-4">
       <ForYou />
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
       <Missed />
+      </div>
     </div>
   )
 }
--- blog-homepage/src/ForYou.jsx
+++ blog-homepage/src/ForYou.jsx
@@ -5,29 +5,40 @@ function ForYou() {
   return(
     <>
       {yourArticles.map((article) => (
-        <div key={article.id} className="card mb-3 Card-width">
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src={article.image} className="img-fluid" alt="Article image"/>
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <a className="card-title Title" href={article.link}>{article.title}</a>
-                <p className="card-text">{article.description}</p>
+        <div key={article.id} className="col">
+          <div key={article.id} className="card h-100 mb-3">
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img 
+                  src={article.image} 
+                  className="img-fluid" 
+                  alt="Article image"/>
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <a 
+                    className="card-title Title" 
+                    href={article.link}>{article.title}
+                  </a>
+                  <p className="card-text">{article.description}</p>
+                </div>
+              </div>
+            <div className="card-footer row">
+              <div className="col-md-4">
+                <img 
+                className="Author-pic" 
+                src={article.author.image} 
+                alt="Image of author"/>
+              </div>
+              <div className="col-md-8"> 
+                <small className="text-muted">
+                  <p >{article.author.name}</p>
+                  <p>{article.postedDate}</p>
+                  <p>{article.minutesToRead}-minute read</p>
+                </small>
               </div>
             </div>
-          <div className="card-footer row">
-            <div className="col-md-4">
-              <img className="Author-pic" src={article.author.image} alt="Image of author"/>
             </div>
-            <div className="col-md-8"> 
-              <small className="text-muted">
-                <p >{article.author.name}</p>
-                <p>{article.postedDate}</p>
-                <p>{article.minutesToRead}-minute read</p>
-              </small>
-            </div>
-          </div>
           </div>
         </div>
     ))}
