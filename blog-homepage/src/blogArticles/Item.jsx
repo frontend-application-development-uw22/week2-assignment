47a57bb4f91108fcda37ea674276b1a57732dd50
pablomarcel <pablomarcel@outlook.com>
2022-05-01 13:01:00 -0700

    cosmetic changes

--- blog-homepage/src/blogArticles/Item.jsx
+++ blog-homepage/src/blogArticles/Item.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 function Item({item, idx}){
 
-  return <li key={idx}>
-    <span>{item.title}</span></li>
+  const date = Date.parse(item.postedDate)
+  const dateObject = new Date(date)
+
+  return (
+    <li key={idx}>
+      <span>
+        <a href={item.link}>{item.title} </a>
+      </span>
+      <span>{item.author.name}</span>
+      <img src={item.image}/>
+      <p>{item.description}</p>
+      {/*<span>{item.title}</span>*/}
+      <span>{dateObject.toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'long',
+      })}</span>
+      <span> · </span>
+      <span>{item.minutesToRead} Minute Read</span>
+
+    </li>
+
+    )
+
 
 }
 
