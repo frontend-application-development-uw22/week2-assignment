80c2d0f6f0c37c734675cd47e09dcf22706c9fcf
Sonal Gupta <sonalaggarwal555@gmail.com>
2022-05-01 16:33:56 -0700

    Week 2 Assignment

---
+++ blog-homepage/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import missedArticles from './missed-articles.json';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <missedArticles/>
+    </div>
+  );
+}
+
+export default App;
