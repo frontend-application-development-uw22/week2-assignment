94bad223e1c0cbdcb6674772a7b701276de8a443
pablomarcel <pablomarcel@outlook.com>
2022-05-02 17:43:29 -0700

    resolved eslint issues. final commit

--- blog-homepage/src/App.js
+++ blog-homepage/src/App.js
@@ -2,30 +2,38 @@ import './App.css';
 import PropTypes from 'prop-types';
 import articles from './_data/your-articles.json';
 import missedArticles from './_data/missed-articles.json';
-// eslint-disable-next-line no-unused-vars,import/extensions,import/no-unresolved
 import MyArticles from './blogArticles/MyArticles';
-// eslint-disable-next-line no-unused-vars,import/extensions,import/no-unresolved
+
 import MissedArticles from './blogArticles/MissedArticles';
 
 function App() {
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div>
 
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <div>
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <h1>For You</h1>
 
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <hr />
 
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <MyArticles articles={articles}/>
 
       </div>
 
+      {/* eslint-disable-next-line react/react-in-jsx-scope */}
       <div>
 
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <h1>In Case You Missed It</h1>
 
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <hr />
 
+        {/* eslint-disable-next-line react/react-in-jsx-scope */}
         <MissedArticles missedArticles={missedArticles}/>
 
       </div>
