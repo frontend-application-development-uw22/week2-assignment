27682e1484a45b2dfa37f82ae067454701548962
Ryan Ku <rjhk@uw.edu>
2022-05-02 17:13:14 -0700

    created BlogHomepageApp

--- blog-homepage/src/App.js
+++ blog-homepage/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import BlogHomepageApp from './BlogHomepageApp/BlogHomepageApp.jsx';
+import missedArticles from './missed-articles.json';
+import yourArticles from './your-articles.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BlogHomepageApp />
     </div>
   );
 }
---
+++ blog-homepage/src/BlogHomepageApp/BlogHomepageApp.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function BlogHomepageApp() {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default BlogHomepageApp;
